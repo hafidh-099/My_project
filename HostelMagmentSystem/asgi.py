c022384e002870170b413c5a46f26adb97758d1f
h4finho <h4finho@kali.h4fidh>
2025-02-02 15:13:35 +0300

    Frotend

--- HostelMagmentSystem/asgi.py
+++ HostelMagmentSystem/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HostelMagmentSystem.settings')
-
+setting_module= 'HostelMagmentSystem.deployment' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'HostelMagmentSystem.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
 application = get_asgi_application()
