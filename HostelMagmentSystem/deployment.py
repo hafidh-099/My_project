0e3becc39742549f2003c3a2ee64eba31ac7ac74
h4finho <h4finho@kali.h4fidh>
2025-02-02 15:56:48 +0300

    Frotend

--- HostelMagmentSystem/deployment.py
+++ HostelMagmentSystem/deployment.py
@@ -35,20 +35,12 @@ STORAGES = {
 }
 
 
-CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTION_STRING']
-CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': CONNECTION_STR['dbname'], 
-        'HOST': CONNECTION_STR['host'], 
-        'USER': CONNECTION_STR['user'],
-        'PASSWORD': CONNECTION_STR['password'],
-        
-    }
+DATABASES ={
+    'default':dj_database_url.config(
+        default=os.environ['DATABASE_URL'],
+        conn_max_age=600
+    )
 }
-
 STATIC_ROOT =  BASE_DIR / 'staticfiles'
 
 
